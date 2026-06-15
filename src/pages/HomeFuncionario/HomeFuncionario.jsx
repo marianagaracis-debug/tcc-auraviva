860706b31ea6eb32d51441efc7d01180243a1efc
Mariana Garacis Costa <marianagaracis@gmail.com>
2026-06-15 15:07:32 +0000

    Menu e Home do funcionário em construção

---
+++ src/pages/HomeFuncionario/HomeFuncionario.jsx
@@ -0,0 +1,17 @@
+import MenuFuncionario from "../MenuFuncionario/MenuFuncionario"
+
+const HomeFuncionario = () => {
+
+return(
+
+    <div>
+
+    <MenuFuncionario/>
+
+
+    </div>
+)
+
+}
+
+export default HomeFuncionario
