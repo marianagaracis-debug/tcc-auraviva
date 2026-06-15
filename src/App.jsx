0e9f2504dfb754d04d879ef5f8256b663e7e0e71
Mariana Garacis Costa <marianagaracis@gmail.com>
2026-06-15 15:23:38 +0000

    Sistema de rotas em construção

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
-import './App.css'
+//import './App.css'
+
 import HomeFuncionario from './pages/HomeFuncionario/HomeFuncionario'
 
 function App() {
---
+++ src/pages/ListarProduto/ListarProduto.jsx
@@ -0,0 +1,14 @@
+import MenuFuncionario from "../MenuFuncionario/MenuFuncionario"
+
+const ListarProduto = () => {
+
+return(
+<div className="container">
+        <MenuFuncionario/>
+
+</div>
+
+)
+}
+
+export default ListarProduto
